a72597581fe4dbc0520619cb76567f9897ef83cd
Jorge <jorgegonzalezburgos2004@gmail.com>
2026-03-14 16:50:30 +0100

    app_index.js y app_mes.js terminados, css ampliado y retoques en html. TODO: README de la práctica.

--- js/app_index.js
+++ js/app_index.js
@@ -1,21 +1,78 @@
 const btnLista = document.querySelector("#btnLista");
-const btnLimpiar = document.querySelector("#btnLimppiar");
+const btnLimpiar = document.querySelector("#btnLimpiar");
 const lista = document.querySelector("#listarTareas");
+const cuadroMes = document.querySelectorAll(".cuadroMes");
 
-btnLista.addEventListener("click",() =>{
-    lista.innerHTML=`<h2>Lista de tareas:</h2>`
-    let hayTarea = false;
+const KEY_CALENDAR = "calendarioNotas123";
+let allNotas=null;
+const arrayMes = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
 
-    //TODO: ¿Cómo cargo la lista de tareas del app_mes.js? (investigar)
-    //TODO: si la lista de tareas tiene contenido -> innerHTML con la lista de tareas
+function init(){
 
-    if (!hayTarea){
-        lista.innerHTML = `<p>Sin tareas</p>`;
+    allNotas=cargarNotas();
+
+    cuadroMes.forEach((cuadro, index) =>{
+        let notasMes = [];
+
+        allNotas.forEach(nota =>{
+            if (nota.mes === index){
+                notasMes.push(nota);
+            }
+        });
+
+        const infoTarea = cuadro.querySelector(".tareasInfo");
+
+        if(notasMes.length > 0){
+            cuadro.classList.add("conNotas");
+            cuadro.classList.remove("sinNotas");
+            infoTarea.textContent = `${notasMes.length} tarea(s) pendiente(s)`;
+        }else{
+            cuadro.classList.add("sinNotas");
+            cuadro.classList.remove("conNotas");
+            infoTarea.textContent = "0 tareas pendientes";
+        }
+    });
+
+}
+
+function cargarNotas(){
+    const datos = localStorage.getItem(KEY_CALENDAR);
+    return datos ? JSON.parse(datos) : [];
+}
+
+btnLista.addEventListener("click", ()=>{ 
+
+    allNotas=cargarNotas();
+
+    if (allNotas.length === 0){
+        lista.innerHTML += "<h3>Sin notas guardadas</h3>";
+        return;
     }
+
+    lista.innerHTML = "<h3>Lista global de tareas:</h3>";
+
+    let ul = document.createElement("ul");
+    allNotas.forEach(nota =>{
+        let li = document.createElement("li");
+        li.innerHTML = `
+            <div class="nota">
+                <strong>${arrayMes[nota.mes]}:</strong> ${nota.titulo} - ${nota.desc}
+            </div>
+        `;
+        ul.appendChild(li);
+    });
+
+    lista.appendChild(ul);
 });
 
-btnLimpiar.addEventListener("click",() =>{
-    if (confirm("Esta acción borrará por completo TODAS las tareas, ¿estás seguro que deseas continuar?")){
-        //TODO: cuando consiga consultar la lista de tareas de app_mes.js, borrar los datos (localStorage.removeItem())
+btnLimpiar.addEventListener("click",() =>{ 
+    
+    if (confirm("¿Estás seguro que desea eliminar todas las notas del calendario?")){
+        localStorage.removeItem(KEY_CALENDAR);
+        lista.innerHTML="";
+        init();
     }
+
 });
+
+init();
--- js/app_mes.js
+++ js/app_mes.js
@@ -1,61 +1,132 @@
 const params = new URLSearchParams(window.location.search);
-const mesAhora = params.get("mes");
-const inputNota = document.querySelector("#nuevaNota");
-const btnAdd = document.querySelector("#addNota");
+const mesAhora = Number(params.get("mes"));
+const KEY_CALENDAR = "calendarioNotas123";
+
+const formNota = document.querySelector("#formularioNotas");
+const tituloNota = document.querySelector("#tituloNota");
+const descNota = document.querySelector("#descNota");
+const errorMsj = document.querySelector("#errorMensaje");
 const listaNotas = document.querySelector("#listaNotas");
-const nombreMes = document.querySelector("#nombreMes");
-const borrarGeneral = document.querySelector("#btnBorrarGeneral");
+const btnAdd = document.querySelector("#addNota");
+
+const arrayMes = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+let allNotas = cargarNotas();
+let editando=null;
 
 function cargarMes(mes){
-    arrayMes = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
     return arrayMes[mes];
 }
 nombreMes.innerHTML=cargarMes(mesAhora);
 
-//const btnBorrar = document.querySelector("#btnBorrar");
+function cargarNotas(){
+    const datos = localStorage.getItem(KEY_CALENDAR);
+    return datos ? JSON.parse(datos) : [];
+}
 
-//const KEY_CALENDAR = "calendarioNotas123";
+function guardarNotas(notas){
+    localStorage.setItem(KEY_CALENDAR, JSON.stringify(notas));
+}
 
-/*
-//esto para el localStorage
-function cargarMes(){
-    const raw = localStorage.getItem(KEY_CALENDAR);
+function renderNotas(){
+    const notasMes = [];
 
-    if(!raw) return [];
+    allNotas.forEach(nota =>{
+        if (nota.mes === mesAhora){
+            notasMes.push(nota);
+        }
+    });
 
-    try {
-        const data = JSON.parse(raw);
-        return Array.isArray(data) ? data : [];
-
-    }catch(error){
-        console.error("JSON inválido", error);
-        return[];
-    }
-};
-*/
-
-btnAdd.addEventListener("click",() =>{
-    const li = document.createElement("li");
-    if (inputNota.value.trim() != ""){
-        li.innerHTML = `
-        <span>${inputNota.value}</span>
+    listaNotas.innerHTML = "";
+    notasMes.forEach(nota => {
+        const li = document.createElement("li");
+        li.innerHTML= `
+            <div class="nota">
+                <div>
+                    <h3>${nota.titulo}</h3>
+                    <p>${nota.desc}</p>
+                </div>
+                <button class ="btnEditar" onclick="editarNota(${nota.id})">Editar</button>
+                <button class ="btnBorrar" onclick="eliminarNota(${nota.id})">Eliminar</button>
+            </div>
         `;
         listaNotas.appendChild(li);
-        inputNota.value="";
-    
+    });
+}
+
+formNota.addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+    const titulo = tituloNota.value.trim();
+    const desc = descNota.value.trim();
+
+    if (titulo ==="" || desc ===""){
+        errorMsj.innerHTML=`<h3>El título y la descripción son obligatorios.</h3>`
+        return;
     }
+
+    if (editando !== null){
+        allNotas.forEach(nota =>{
+            if (nota.id === editando){
+                nota.titulo = titulo;
+                nota.desc = desc;
+            }
+        });
+
+        editando = null;
+        btnAdd.textContent="Añadir nota";
+
+    }else{
+        const nuevaNota = {
+            id: Date.now(),
+            mes: mesAhora,
+            titulo: titulo,
+            desc: desc
+        };
+        allNotas.push(nuevaNota);
+    }
+
+    guardarNotas(allNotas);
+    formNota.reset();
+    errorMsj.innerHTML="";
+    renderNotas();
+
 });
 
-//TODO: crear boton dentro de la lista para borrar solo esa tarea
-function borrarLista(){
+window.eliminarNota = function(id){
+    if (confirm("¿Está seguro que desea borrar esta nota?")){
+        
+        let notasNoBorrar = [];
 
+        allNotas.forEach(nota =>{
+            if (nota.id !== id){
+                notasNoBorrar.push(nota);
+            }
+        });
+
+        allNotas = notasNoBorrar;
+        guardarNotas(allNotas);
+        renderNotas();
+    }
 };
 
-//TODO: crear boton dentro de la lista para editar el contenido de esa tarea
-function editarLista(){
+window.editarNota = function(id){
+    let editNota;
 
-};
+    allNotas.forEach(nota =>{
+        if (nota.id === id){
+            editNota = nota;
+        }
+    });
 
-borrarGeneral.addEventListener("click",() => {
+    if (editNota !== null){
+        tituloNota.value = editNota.titulo;
+        descNota.value = editNota.desc;
 
-});
+        editando=id;
+
+        btnAdd.textContent = "Guardar cambios";
+
+    }
+}
+
+renderNotas();
